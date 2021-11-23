a818f668967228cfc806a5488b3419ff15dbcdee
leontevsky <alexander.leontevsky@gmail.com>
2021-11-23 19:20:41 +0300

    Контекст в Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { CurrentUserContext } from '../context/CurrentUserContext.js';
+import api from '../utils/Api.js';
 
-function Card({ card, onCardClick }) {
+function Card({ card, onCardClick, onCardLike }) {
   const currentUser = React.useContext(CurrentUserContext);
-
   // Определяем, являемся ли мы владельцем текущей карточки
   const isOwn = card.owner._id === currentUser._id;
 
   // Создаём переменную, которую после зададим в `className` для кнопки удаления
-  const cardDeleteButtonClassName = `card__delete-button ${
-    isOwn ? 'card__delete-button_visible' : 'card__delete-button_hidden'
+  const cardDeleteButtonClassName = `element__button_delete ${
+    isOwn ? 'element__button_delete' : 'element__button_delete-hidden'
   }`;
 
   // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
   const isLiked = card.likes.some((i) => i._id === currentUser._id);
 
   // Создаём переменную, которую после зададим в `className` для кнопки лайка
-  const cardLikeButtonClassName = `...`;
+  const cardLikeButtonClassName = `element__button ${isLiked ? 'element__button_theme-dark' : 'element__button'}`;
 
   // вместо пропса используем конткретные слова
   function handleClick() {
     onCardClick(card);
+  }
+
+  function handleLikeClick() {
+    onCardLike(card);
   }
 
   return (
@@ -29,11 +33,11 @@ function Card({ card, onCardClick }) {
       <div className="element__caption">
         <h2 className="element__title">{card.name}</h2>
         <div className="element__actions">
-          <button type="button" className="element__button" id="like"></button>
+          <button type="button" className={cardLikeButtonClassName} id="like"></button>
           <p className="element__button_count">{card.likes.length}</p>
         </div>
       </div>
-      <button type="button" className="element__button_delete"></button>
+      <button type="button" className={cardDeleteButtonClassName}></button>
     </li>
   );
 }
